18e6a7d7fc8cce6270fe053619e7c8cd914a5ba0
melgacorm <rmmelgaco@gmail.com>
2024-03-19 11:40:02 -0300

    listando atrações turísticas

---
+++ src/components/CardAtracaoTuristica.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps(['atracaoTuristica'])
+</script>
+<template>
+  <section>
+    <h2> {{ atracaoTuristica.nome }}</h2>
+    <p class='descricaoAtracao'>{{ atracaoTuristica.descricao }}</p>
+    <p class='footerCardAtracao'>{{ atracaoTuristica.visitacao }}</p>
+  </section>
+</template>
+
+<style scoped>
+section {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid lightgray;
+  border-radius: 10px;
+  box-shadow: 2px 2px 2px lightgray;
+}
+
+section > .descricaoAtracao {
+  font-size: 1.2rem;
+  text-indent: 2rem;
+  text-align: justify;
+}
+
+section > .footerCardAtracao {
+  text-align: right;
+}
+</style>
--- src/pages/PaginaAtracoesTuristicas.vue
+++ src/pages/PaginaAtracoesTuristicas.vue
@@ -1,5 +1,18 @@
+<script setup>
+import {ref} from 'vue';
+import CardAtracaoTuristica from '@/components/CardAtracaoTuristica.vue';
+
+let atracoesTuristicas = ref([])
+
+fetch('/atracoes.json')
+    .then(resposta => resposta.json())
+    .then(dados => atracoesTuristicas.value = dados)
+</script>
 <template>
   <h1>Atrações turísticas</h1>
+  <p>Total: {{ atracoesTuristicas.length }}</p>
+  <CardAtracaoTuristica
+      v-for='(atracaoTuristica, indice) in atracoesTuristicas'
+      :key='`atracaoTuristica${indice}`'
+      :atracaoTuristica='atracaoTuristica'/>
 </template>
-<script setup>
-</script>
